b8acd82fe258e3c8cdff904d0c01f7efd27b882d
Riadz <dzriaddz@gmail.com>
2020-08-28 17:07:29 +0100

    teacher dashboard tweaks

--- teacher_dashboard.php
+++ teacher_dashboard.php
@@ -16,16 +16,23 @@ require('common/header.php')
 			<div class="posts row">
 				<div class="posts-card-container col-lg-6">
 					<div class="posts-card">
-						<div class="posts-card-actions">
-							<a href="#" title="Voir" class="posts-card-actions-btn">
-								<i class="fas fa-eye"></i>
-							</a>
-							<a href="#" title="Modifier" class="posts-card-actions-btn">
-								<i class="fas fa-pencil-alt"></i>
-							</a>
-							<button title="supprimer" class="posts-card-actions-btn">
-								<i class="fas fa-times"></i>
-							</button>
+						<div class="posts-card-header">
+							<div class="posts-card-status">
+								<h3>Ètat: </h3>
+								<span class="status-ouvert">ouvert</span>
+							</div>
+
+							<div class="posts-card-actions">
+								<a href="#" title="Voir" class="posts-card-actions-btn">
+									<i class="fas fa-eye"></i>
+								</a>
+								<a href="#" title="Modifier" class="posts-card-actions-btn">
+									<i class="fas fa-pencil-alt"></i>
+								</a>
+								<button title="supprimer" class="posts-card-actions-btn">
+									<i class="fas fa-times"></i>
+								</button>
+							</div>
 						</div>
 						<span class="posts-card-title">
 							Titre De l'annonce !
@@ -61,50 +68,6 @@ require('common/header.php')
 								<div class="posts-card-themes-card">
 									<h4>theme 4</h4>
 									<span>2</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="posts-card-container col-lg-6">
-					<div class="posts-card">
-						<div class="posts-card-actions">
-							<a href="#" title="Voir" class="posts-card-actions-btn">
-								<i class="fas fa-eye"></i>
-							</a>
-							<a href="#" title="Modifier" class="posts-card-actions-btn">
-								<i class="fas fa-pencil-alt"></i>
-							</a>
-							<button title="supprimer" class="posts-card-actions-btn">
-								<i class="fas fa-times"></i>
-							</button>
-						</div>
-						<span class="posts-card-title">
-							Titre De l'annonce !
-						</span>
-						<div class="posts-card-info row">
-							<div class="col-6">
-								<h3>Faculté:</h3>
-								<div class="posts-card-info-fac">
-									<span>Sci Ingéniorat</span>
-									<img src="img/facs/fac-1.png" alt="">
-								</div>
-							</div>
-							<div class="col-6 d-flex flex-column">
-								<h3>Departement:</h3>
-								<span class="posts-card-info-dep">Informatique</span>
-							</div>
-						</div>
-						<div class="posts-card-themes">
-							<h3>Etudiant encadré dans chaque theme</h3>
-							<div class="posts-card-themes-container">
-								<div class="posts-card-themes-card">
-									<h4>theme 1</h4>
-									<span>5</span>
-								</div>
-								<div class="posts-card-themes-card">
-									<h4>theme 2</h4>
-									<span>4</span>
 								</div>
 							</div>
 						</div>
